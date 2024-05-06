b8aa335a97a79b05026b1b0e53da908def5371b6
MaulanaNaz <mlnnzmdn@gmail.com>
2024-05-06 15:17:55 +0700

    latihan

---
+++ latihan_css_2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS Cards</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-img">
+                <img src="jimmy.jpg" alt="">
+            </div>
+
+            <div class="card-body">
+                <h2 class="card-title">Drive (2020)</h2>
+                <p class="card-intro">
+                    Drive is Ready. OH MY GOOD. "Kamu tidak bisa memperlakukan mobil seperti pasien. Sebuah mobil membutuhkan cinta." 
+                </p>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
